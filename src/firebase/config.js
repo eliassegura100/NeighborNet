30e50eb9fc5a235413f06d93050ead6901217406
Garnik Gevorkyan <garnikgevorkyan23@gmail.com>
2025-11-08 16:15:50 -0800

    Added more backend functionality and twilio info

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,18 +1,27 @@
-import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyBuufYBxJOl1QsrNA3hiZQJWUllEvB4vVs",
+  authDomain: "neighbornet-57018.firebaseapp.com",
+  projectId: "neighbornet-57018",
+  storageBucket: "neighbornet-57018.firebasestorage.app",
+  messagingSenderId: "787592606949",
+  appId: "1:787592606949:web:270f40261ef9684efc100c",
+  measurementId: "G-LLMTJQ5HVL"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
